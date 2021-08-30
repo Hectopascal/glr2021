05463d8f0ce0e9078508a7219a3da52adf9121af
Yan <voracian@gmail.com>
2021-08-30 17:42:32 +0100

    added landmark dataset

---
+++ base/base_datasets.py
@@ -0,0 +1,37 @@
+import os
+import torch
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+
+class LandmarksDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+
+        self.labels = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        label = self.img_labels.iloc[idx, 1]
+        img_name = os.path.join(self.root_dir, self.labels.iloc[idx, 0])
+        image = Image.open(img_name).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
